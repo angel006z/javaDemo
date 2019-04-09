4e5f2f8ef3ea333add4a8510199080533cb83a0e
angel006z <angel006z@163.com>
2019-04-09 09:14:56 +0800

    1

--- src/main/java/com/meida/common/util/RegexValidate.java
+++ src/main/java/com/meida/common/util/RegexValidate.java
@@ -1,10 +1,35 @@
 package com.meida.common.util;
 
+import java.util.UUID;
+
 public class RegexValidate {
 
-	public static boolean isGuid(String string) {
-		// TODO Auto-generated method stub
+	public static boolean isGuid(String uuid) {
+		if(StringUtils.isEmpty(uuid)){
+			return false;
+		}
+		String regex = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$";
+		if (uuid.matches(regex)) {
+			return true;
+		}
 		return false;
 	}
 
+	public static void main(String[] args) {
+		String uuid1 = "e65deb4c-a110-49c8-a4ef-6e69447968d6";
+		String uuid2 = "ca4a8a92d4ed4fc48a4f345c587fbdcb";
+		String uuid3 = "e1f15f1d-6edb-4f70-8a05465se273eaf95a";
+		System.out.println("check > " + uuid1 + " > " + isGuid(uuid1));
+		System.out.println("check > " + uuid2 + " > " + isGuid(uuid2));
+		System.out.println("check > " + uuid3 + " > " + isGuid(uuid3));
+
+		for (int i = 0; i < 100000 ; i++) {
+			String uuid4= UUID.randomUUID().toString();
+			if(!isGuid(uuid4)){
+			System.out.println("check > " + uuid4 + " > " + isGuid(uuid4));
+			}
+		}
+
+		System.out.println("check OK");
+	}
 }
