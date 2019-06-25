1ecb1d6f9cfa4fcdb343c583e014a932797e0a0d
angel006z <angel006z@163.com>
2019-06-25 21:09:52 +0800

    123

--- src/main/java/com/meida/pay/alipay/page/service/impl/AlipayPageTradeServiceImpl.java
+++ src/main/java/com/meida/pay/alipay/page/service/impl/AlipayPageTradeServiceImpl.java
@@ -4,9 +4,11 @@ import com.alipay.api.AlipayClient;
 import com.alipay.api.request.AlipayTradePagePayRequest;
 import com.alipay.api.request.AlipayTradePrecreateRequest;
 import com.alipay.api.request.AlipayTradeQueryRequest;
+import com.alipay.api.request.AlipayTradeRefundRequest;
 import com.alipay.api.response.AlipayTradePagePayResponse;
 import com.alipay.api.response.AlipayTradePrecreateResponse;
 import com.alipay.api.response.AlipayTradeQueryResponse;
+import com.alipay.api.response.AlipayTradeRefundResponse;
 import com.meida.base.domain.vo.ResultMessage;
 import com.meida.common.util.JsonUtils;
 import com.meida.common.util.constant.EErrorCode;
@@ -97,6 +99,45 @@ public class AlipayPageTradeServiceImpl implements IAlipayPageTradeService {
             } else {
                 result.setCode(EErrorCode.Error);
                 result.setMessage("查询支付宝交易记录失败.");
+            }
+            return result;
+        } catch (Exception ex) {
+            logger.warn(String.format("调用支付宝接口 参数信息：%s,异常信息：%s" + JsonUtils.toJSONString(builder), ex.getMessage()));
+            // 请求支付过程中异常，请重新操作.
+            result.setCode(EErrorCode.Error);
+            result.setMessage("调用支付宝接口异常，请返回重新操作。");
+            return result;
+        }
+    }
+
+    @Override
+    public AlipayPageResultTradeRefund tradeRefund(AlipayPageParametersTradeRefund builder) {
+        AlipayPageResultTradeRefund result = new AlipayPageResultTradeRefund();
+        try {
+            AlipayTradeRefundRequest request = new AlipayTradeRefundRequest();
+            request.setBizContent(builder.GetParameters());
+            AlipayTradeRefundResponse response = alipayClient.execute(request);
+            if (response.isSuccess()) {
+                result.setTrade_no(response.getTradeNo());
+                result.setOut_trade_no(response.getOutTradeNo());
+                result.setBuyer_logon_id(response.getBuyerLogonId());
+                result.setFund_change(response.getFundChange());
+                result.setRefund_fee(response.getRefundFee());
+                result.setRefund_currency(response.getRefundCurrency());
+                result.setGmt_refund_pay(response.getGmtRefundPay());
+                result.setRefund_detail_item_list(JsonUtils.toJSONString(response.getRefundDetailItemList()));
+                result.setStore_name(response.getStoreName());
+                result.setBuyer_user_id(response.getBuyerUserId());
+                result.setRefund_preset_paytool_list(JsonUtils.toJSONString(response.getRefundPresetPaytoolList()));
+                result.setRefund_settlement_id(response.getRefundSettlementId());
+                result.setPresent_refund_buyer_amount(response.getPresentRefundBuyerAmount());
+                result.setPresent_refund_discount_amount(response.getPresentRefundDiscountAmount());
+                result.setPresent_refund_mdiscount_amount(response.getPresentRefundMdiscountAmount());
+                result.setCode(EErrorCode.Success);
+                result.setMessage("操作成功.");
+            } else {
+                result.setCode(EErrorCode.Error);
+                result.setMessage("调用支付宝接口失败，请重新操作.");
             }
             return result;
         } catch (Exception ex) {
--- src/main/java/com/meida/pay/alipay/page/service/inter/IAlipayPageTradeService.java
+++ src/main/java/com/meida/pay/alipay/page/service/inter/IAlipayPageTradeService.java
@@ -31,5 +31,17 @@ public interface IAlipayPageTradeService {
      */
 	ResultMessage tradeIsPaySuccess(AlipayPageParametersTradeQuery builder);
 
-
+    /**
+     * 统一收单交易退款接口
+     * 当交易发生之后一段时间内，由于买家或者卖家的原因需要退款时，
+     * 卖家可以通过退款接口将支付款退还给买家，支付宝将在收到退款
+     * 请求并且验证成功之后，按照退款规则将支付款按原路退到买家帐
+     * 号上。 交易超过约定时间（签约时设置的可退款时间）的订单无法
+     * 进行退款 支付宝退款支持单笔交易分多次退款，多次退款需要提交
+     * 原支付订单的商户订单号和设置不同的退款单号。一笔退款失败后重
+     * 新提交，要采用原来的退款单号。总退款金额不能超过用户实际支付金额
+     * @param builder
+     * @return
+     */
+    AlipayPageResultTradeRefund tradeRefund(AlipayPageParametersTradeRefund builder);
 }
